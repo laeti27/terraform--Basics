0523a9b767b8672ef69afe478a74a2b007217d3f
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-28 09:05:48 -0600

    Terraform Basics

--- 02-vars/output.tf
+++ 02-vars/output.tf
@@ -10,3 +10,10 @@ variable "bacth" {
 output "batch-op" {
     value = var.bacth
 }
+
+# when enclose a variable in quotes and when to use $ notation for a variable ??
+# whenever your variable is enclosed in a set of strings or in between a sentence, then we to enclose then in strings and with bash notation
+
+output "batch_number" {
+    value = "Curent Batch is var.batch"
+}
