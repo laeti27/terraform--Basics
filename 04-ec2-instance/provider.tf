6567038f57804709d347448abe061c57414a4097
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-28 15:51:51 -0600

    Terraform Basics

--- 04-ec2-instance/provider.tf
+++ 04-ec2-instance/provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "app" {
   ami           = "ami-0f75a13ad2e340a58"       # argument
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["aws_security_group.allow_lts.id"] 
+  vpc_security_group_ids = [aws_security_group.allow_lts.id] 
 
   tags = {
     Name = "DevOps-With-AWS-LabInstance"
