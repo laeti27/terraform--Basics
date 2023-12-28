6010e2fe595969d82f561acc2f0c4255901d7447
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-28 14:49:10 -0600

    Terraform Basics

--- 04-ec2-instance/provider.tf
+++ 04-ec2-instance/provider.tf
@@ -2,11 +2,21 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0f75a13ad2e340a58"
+resource "aws_instance" "app" {
+  ami           = "ami-0f75a13ad2e340a58"       # argument
   instance_type = "t3.micro"
 
   tags = {
     Name = "DevOps-With-AWS-LabInstance"
   }
 }
+
+output "instance_dns" {
+  value = aws_instance.app.private_dns                                   # attributes
+}
+
+
+output "instance_arn" {
+  value = aws_instance.app.arn
+}
+                  
