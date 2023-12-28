ad601195ec24822e9f49fb69c52b686a4de0295b
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-28 12:13:59 -0600

    Terraform Basics

--- 03-ec2-instance/ec.tf
+++ 03-ec2-instance/ec.tf
@@ -1,8 +1,8 @@
-resource "[aws_instance]" "app" {
-    ami           = "ami-0f75a13ad2e340a58"
-    instance_type = "t2.micro"
+resource "aws_instance" "app" {
+  ami           = "ami-0f75a13ad2e340a58"
+  instance_type = "t2.micro"
 
-    tags = {
-        Name = "HelloWord"
-    }
+  tags = {
+    Name = "HelloWorld"
+  }
 }
