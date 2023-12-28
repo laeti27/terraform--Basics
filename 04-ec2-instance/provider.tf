269daf373326535018422e4530d80bf2ced0fb39
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-28 14:03:00 -0600

    Terraform Basics

--- 04-ec2-instance/provider.tf
+++ 04-ec2-instance/provider.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "app" {
   ami           = "ami-0f75a13ad2e340a58"
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
 
   tags = {
     Name = "Hello World"
