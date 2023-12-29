66db71ebe283501990afd4fab4acd984b63822bf
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-28 23:15:52 -0600

    Terraform Basics

--- 04-ec2-instance/provider.tf
+++ 04-ec2-instance/provider.tf
@@ -1,9 +1,5 @@
-provider "aws" {
-    region = "us-east-1"
-}
-
 resource "aws_instance" "app" {
-  ami           = "ami-0f75a13ad2e340a58"       # argument
+  ami           = "ami-0f75a13ad2e340a58"                                  # argument
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_lts.id] 
 
@@ -22,22 +18,22 @@ output "instance_arn" {
 }
 
 resource "aws_security_group" "allow-tls" {
-  name         = "b56_allow_tls"
-  description  = "B56 Allow TLS inbound traffic"
+  name               = "b56_allow_tls"
+  description        = "B56 Allow TLS inbound traffic"
 
   ingress {
-    description     = "SSH from VPC"
-    from_port       = 22
-    to_port         = 22
-    protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]
+      description     = "SSH from VPC"
+      from_port       = 22
+      to_port         = 22
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+      from_port       = 0
+      to_port         = 0
+      protocol        = "-1"
+      cidr_blocks     = ["0.0.0.0/0"]
   }
 
   tags ={
