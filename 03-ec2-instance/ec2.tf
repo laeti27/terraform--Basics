7de36b862fa1e89b8c8121a11a57f297fedfd86f
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-30 11:08:48 -0600

    Terraform Basics

--- 03-ec2-instance/ec2.tf
+++ 03-ec2-instance/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "app" {
   ami           = "ami-0f75a13ad2e340a58"                                                       # argument
   instance_type = "t3.micro"
-  vpc_security_group_ids = ["aws_security_group.allow_tls.id"]
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {
     Name = "DevOps laeti"
