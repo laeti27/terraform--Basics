8430b8d5bfaf001d5f1994c0cfe20f9feee54ff5
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-30 11:56:59 -0600

    Terraform Basics

--- 03-ec2-instance/ec2.tf
+++ 03-ec2-instance/ec2.tf
@@ -8,39 +8,3 @@ resource "aws_instance" "app" {
   }
 }
 
-output "instance_dns" {
-    value = aws_instance.app.private_dns                                                     # attribut
-}
-
-
-
-output "instance_arn" {
-    value = aws_instance.app.arn                                                                 # attribut
-}
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  
-
-  ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
---
+++ 04-DataSource/ec2.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "app" {
+  ami           = data.aws_ami.ami.id                                                       # argument
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name = "DevOps laeti"
+  }
+}
+
+
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "DevOps-LabImage-CentOS7"
+  owners           = ["750773811746"]
+  }
+
+  
