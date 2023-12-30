242cab9c4161cfcb9c2eedc335fd958932034978
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-30 12:06:03 -0600

    Terraform Basics

--- 04-DataSource/ec2.tf
+++ 04-DataSource/ec2.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "app" {
 }
 
 
-data "aws_ami" "ami" {
+data "aws_ami" "ami"{
   most_recent      = true
   name_regex       = "DevOps-LabImage-CentOS7"
   owners           = ["750773811746"]
