833be87d58d37da2ab4ac4eba657c6d018468ee9
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-30 10:54:08 -0600

    Terraform Basics

--- 03-ec2-instance/ec2.tf
+++ 03-ec2-instance/ec2.tf
@@ -16,3 +16,30 @@ output "instance_dns" {
 output "instance_arn" {
     value = aws_instance.app.arn                                                                 # attribut
 }
+
+resource "aws_security_group" "allow_tls" {
+  name        = "b56_allow_tls"
+  description = "B56 Allow TLS inbound traffic"
+  
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
