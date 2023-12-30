93f25075da852bee8624dfb896084c0ac4208f67
magnelaetitiakanga <kangalaetitia@yahoo.com>
2023-12-30 10:36:33 -0600

    Terraform Basics

--- 03-ec2-instance/ec2.tf
+++ 03-ec2-instance/ec2.tf
@@ -1,8 +1,18 @@
 resource "aws_instance" "app" {
-  ami           = "ami-0f75a13ad2e340a58"
+  ami           = "ami-0f75a13ad2e340a58"                                                       # argument
   instance_type = "t3.micro"
 
   tags = {
     Name = "DevOps laeti"
   }
+}
+
+output "instance_dns" {
+    value = aws_instances.app.arn.private_dns                                                     # attribut
+}
+
+
+
+output "instance_arn" {
+    value = aws_instances.app.arn                                                                 # attribut
 }
