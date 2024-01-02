cce05a193d5787e8502dffec819d9d46e7da76b3
magnelaetitiakanga <kangalaetitia@yahoo.com>
2024-01-02 11:46:21 -0600

    Terraform Basics

--- 04-DataSource/ec2.tf
+++ 04-DataSource/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "app" {
-  ami           = data.aws_ami.ami.id                                                       # argument
+  ami           = "ami-0f75a13ad2e340a58"                                                      # argument
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
