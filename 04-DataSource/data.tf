0afd0eb032a91ff71660621f7bc246c8a9519a24
magnelaetitiakanga <kangalaetitia@yahoo.com>
2024-01-03 12:28:53 -0600

    auto push

--- 04-DataSource/data.tf
+++
@@ -1,5 +0,0 @@
-data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "ansibleb56"
-  owners           = ["750773811746"]
-}
--- 05-modules/local/ec2.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_instance" "app" {
-  ami           = "ami-0f75a13ad2e340a58"                                                       # argument
-  instance_type = "t2.micro"
-}
