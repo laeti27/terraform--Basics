5c7e2c598ad7ffbc674d107a26053161317b5322
magnelaetitiakanga <kangalaetitia@yahoo.com>
2024-01-02 22:20:26 -0600

    Terraform Basics

--- 04-DataSource/Data.tf
+++ 04-DataSource/Data.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "ami-0b98a32b1c5e0d105"
+  name_regex       = "ami-048d7f5ca8e2edd06"
   owners           = ["750773811746"]
 }
